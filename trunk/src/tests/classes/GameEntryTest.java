414762877e3593ea3f2e3a9dac7e2e2b5b625e06
giovanni.bat@gmail.com <giovanni.bat@gmail.com@61f61afd-5977-cfbf-1cf9-f7827cc3ce70>
2014-05-04 23:54:35 +0000

    Novos testes em GameEntryTest.

--- trunk/src/tests/classes/GameEntryTest.java
+++ trunk/src/tests/classes/GameEntryTest.java
@@ -9,34 +9,55 @@ public class GameEntryTest {
 	
 	
 	@Test
-	public void testEmptyName(){
+	public void testNotEmptyName(){
+		GameEntry gameEntry = new GameEntry("", 100);
 		
+		Assert.assertFalse(gameEntry.getName().isEmpty());
 	}
 	
 	@Test
 	public void testGreatName(){
+		GameEntry gameEntry = new GameEntry("user56789123456789101", 100);
 		
+		Integer length = gameEntry.getName().length();
+		
+		Assert.assertTrue(length < 20);
 	}
 	
 	@Test
-	public void testaSmallNome(){
+	public void testName(){
+		GameEntry gameEntry = new GameEntry("user1", 100);
 		
+		Integer length = gameEntry.getName().length();
+		
+		Assert.assertTrue(length < 20);
 	}
 	
 	@Test
 	public void testMaxScore(){
-		
+		GameEntry gameEntry = new GameEntry("user1", 1001);
+
+		Assert.assertTrue(gameEntry.getScore() <= 1000);
+	}
+	
+	@Test
+	public void testNegativeScore(){
+		GameEntry gameEntry = new GameEntry("user1", -1);
+
+		Assert.assertTrue(gameEntry.getScore() > 0);
 	}
 	
 	@Test
 	public void testScore(){
-	
+		GameEntry gameEntry = new GameEntry("user1", 10);
+ 
+		Assert.assertTrue(gameEntry.getScore() > 0 && gameEntry.getScore() <= 1000);
 	}
 	
 	@Test
 	public void testToString(){
 		GameEntry gameEntry = new GameEntry("user1", 100);
-		String result = "(user1,100)";
+		String result = "(user1, 100)";
 		
 		Assert.assertEquals(gameEntry.toString(), result);
 	}
